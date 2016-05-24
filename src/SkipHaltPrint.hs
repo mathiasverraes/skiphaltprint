ed35de43aedfe09aeca96959af7f3bc308b2e1b8
Mathias Verraes <mathias@verraes.net>
2016-05-24 22:12:47 +0200

    removed var

--- src/SkipHaltPrint.hs
+++ src/SkipHaltPrint.hs
@@ -14,14 +14,14 @@ interp (Halt:_) = ""
 interp (Print s:cmds) = s ++ interp cmds
 
 fizzbuzz :: Int -> Program
-fizzbuzz n = (base n . fizz n . buzz n) [Skip]
+fizzbuzz n = (base . fizz . buzz) [Skip]
   where
-    fizz n
+    fizz
       | n `mod` 3 == 0 = \p -> [Print "Fizz"] ++ p ++ [Halt]
       | otherwise = \p -> p ++ [Skip]
-    buzz n
+    buzz
       | n `mod` 5 == 0 = \p -> [Print "Buzz"] ++ p ++ [Halt]
       | otherwise = \p -> p ++ [Skip]
-    base n = \p -> p ++ [Print (show n)]
+    base = \p -> p ++ [Print (show n)]
 
 
