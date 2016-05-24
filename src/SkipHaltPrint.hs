b3134e47b6fabdb818a5425733c777beb5dec139
Mathias Verraes <mathias@verraes.net>
2016-05-24 23:12:19 +0200

    apply step straight away

--- src/SkipHaltPrint.hs
+++ src/SkipHaltPrint.hs
@@ -2,25 +2,25 @@ module SkipHaltPrint where
 
 data Cmd = Skip | Halt | Print String deriving(Show)
 type Program = [Cmd]
-type Context = Program -> Program
+type Context = String -> String
 
 run :: IO ()
 run = mapM_ (putStr . (++" ") . interp . fizzbuzz) [1..100]
 
-interp :: Program -> String
-interp p = foldr (.) id (fmap step p) ""
+interp :: [Context] -> String
+interp c = foldr (.) id c ""
 
 step :: Cmd -> String -> String
 step Skip x = x
 step Halt _ = ""
 step (Print x) y = x ++ y
 
-fizzbuzz :: Int -> Program
-fizzbuzz n = (base . test 3 "Fizz" . test 5 "Buzz") [Skip]
+fizzbuzz :: Int -> [Context]
+fizzbuzz n = (base . test 3 "Fizz" . test 5 "Buzz") [step Skip]
   where
     test d str
-      | n `mod` d == 0 = \p -> [Print str] ++ p ++ [Halt]
+      | n `mod` d == 0 = \p -> [step $ Print str] ++ p ++ [step Halt]
       | otherwise = id
-    base = \p -> p ++ [Print (show n)]
+    base = \p -> p ++ [step $ Print (show n)]
 
 
