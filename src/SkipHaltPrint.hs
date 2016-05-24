545aee77b196e5b7395a71de265325049efe7a00
Mathias Verraes <mathias@verraes.net>
2016-05-24 23:19:32 +0200

    instead of folding over compose, simple compose directly

--- src/SkipHaltPrint.hs
+++ src/SkipHaltPrint.hs
@@ -5,17 +5,13 @@ type Program = [Cmd]
 type Context = String -> String
 
 run :: IO ()
-run = mapM_ (putStr . (++" ") . interp . fizzbuzz) [1..100]
+run = mapM_ (putStr . (++" ")  . fizzbuzz) [1..100]
 
-interp :: [Context] -> String
-interp c = foldr (.) id c ""
-
-fizzbuzz :: Int -> [Context]
-fizzbuzz n = (base . test 3 "Fizz" . test 5 "Buzz") [id]
+fizzbuzz :: Int -> String
+fizzbuzz n = (test 3 "Fizz" . test 5 "Buzz") id (show n)
   where
     test d str
-      | n `mod` d == 0 = \p -> [(str ++)] ++ p ++ [const ""]
+      | n `mod` d == 0 = \p -> (str ++) . p . const ""
       | otherwise = id
-    base = \p -> p ++ [((show n)++)]
 
 
