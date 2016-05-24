82152682e7a3e4ee99aed26055fe98674204db2e
Mathias Verraes <mathias@verraes.net>
2016-05-24 23:33:38 +0200

    little DSL

--- src/SkipHaltPrint.hs
+++ src/SkipHaltPrint.hs
@@ -1,17 +1,17 @@
 module SkipHaltPrint where
 
-data Cmd = Skip | Halt | Print String deriving(Show)
-type Program = [Cmd]
-type Context = String -> String
-
 run :: IO ()
 run = mapM_ (putStr . (++" ")  . fizzbuzz) [1..100]
 
 fizzbuzz :: Int -> String
-fizzbuzz n = ((3 >=> "Fizz") . (5 >=> "Buzz")) id (show n)
+fizzbuzz n = rules id (show n)
   where
      test d str f
       | n `mod` d == 0 = const(str ++ f "")
       | otherwise = f
      (>=>) = test
 
+     rules =
+          (3 >=> "Fizz")
+        . (5 >=> "Buzz")
+        . (7 >=> "Hiss")
