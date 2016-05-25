23de048a6536e854bf9aedc03a8e027ed3f95db2
Mathias Verraes <mathias@verraes.net>
2016-05-25 09:21:23 +0200

    types

--- src/SkipHaltPrint.hs
+++ src/SkipHaltPrint.hs
@@ -3,9 +3,10 @@ module SkipHaltPrint where
 run :: IO ()
 run = mapM_ (putStr . (++" ")  . fizzbuzz) [1..100]
 
-fizzbuzz :: Int -> String
+fizzbuzz :: Integer -> String
 fizzbuzz n = rules id (show n)
   where
+     test :: Integer -> String -> (String -> String) -> (String -> String)
      test d str f
       | n `mod` d == 0 = const(str ++ f "")
       | otherwise = f
